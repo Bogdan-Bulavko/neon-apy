7aabc5a3bcbfae3e5d320f642e973fd2aa751606
Bogdan <zarechnyy00@internet.ru>
2025-07-20 14:12:37 +0300

    added menu

--- src/pages/Home/src/Home.vue
+++ src/pages/Home/src/Home.vue
@@ -1,15 +1,20 @@
 <script setup>
 import HeaderHome from './components/HeaderHome.vue';
+import MenuHome from './components/MenuHome.vue';
 import RouletteHome from './components/RouletteHome.vue';
 import TimerHome from './components/TimerHome.vue';
 import TotalBank from './components/TotalBank.vue';
 </script>
 
 <template>
-  <HeaderHome></HeaderHome>
-  <TimerHome></TimerHome>
-  <TotalBank></TotalBank>
-  <RouletteHome></RouletteHome>
+  <div class="p-10">
+    <HeaderHome></HeaderHome>
+    <TimerHome></TimerHome>
+    <TotalBank></TotalBank>
+    <RouletteHome></RouletteHome>
+  </div>
+
+  <MenuHome></MenuHome>
 </template>
 
 <style scoped></style>
---
+++ src/pages/Home/src/components/MenuHome.vue
@@ -0,0 +1,62 @@
+<script setup></script>
+
+<template>
+  <menu>
+    <div>
+      <ul class="mb-1 flex justify-center">
+        <li class="relative -translate-x-30">
+          <img src="./public/die.webp" alt="" class="h-16 min-w-70" />
+          <img
+            src="./public/money-turnover.webp"
+            alt="money-turnover"
+            class="w-11 absolute top-2 right-3"
+          />
+        </li>
+        <li class="relative translate-x-30">
+          <img src="./public/die.webp" alt="" class="h-16 min-w-70" />
+          <img
+            src="./public/friends.webp"
+            alt="friends"
+            class="w-11 absolute top-2 left-3"
+          />
+        </li>
+      </ul>
+      <ul class="flex justify-center">
+        <li class="relative -translate-x-30">
+          <img src="./public/die.webp" alt="" class="h-16 min-w-70" />
+          <img
+            src="./public/home.webp"
+            alt="home"
+            class="w-11 absolute top-2 right-3"
+          />
+        </li>
+        <li class="relative translate-x-30">
+          <img src="./public/die.webp" alt="" class="h-16 min-w-70" />
+          <img
+            src="./public/wallet.webp"
+            alt="wallet"
+            class="w-11 absolute top-2 left-3"
+          />
+        </li>
+      </ul>
+      <div class="relative">
+        <img
+          src="./public/lion-trail.webp"
+          alt="hunting"
+          class="block w-21 m-auto"
+        />
+        <p
+          class="absolute top-13 left-1/2 -translate-x-1/2 -translate-y-1/2 text-xl text-[#99abff] text-shadow-xs text-shadow-blue-950 text-stroke-blue"
+        >
+          Охота
+        </p>
+      </div>
+    </div>
+  </menu>
+</template>
+
+<style scoped>
+.text-stroke-blue {
+  -webkit-text-stroke: 1px #3b82f6; /* blue-500 */
+}
+</style>
