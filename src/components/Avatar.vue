67c7283900db4318e88543df6692ccfa0d1ee29a
Bogdan <zarechnyy00@internet.ru>
2025-08-04 21:34:34 +0300

    create tg logo user

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,18 +1,18 @@
 <script setup>
-// import { ref, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 
-// const avatarUrl = ref('/default-avatar.png');
+const avatarUrl = ref('/default-avatar.png');
 
-// onMounted(() => {
-//   const tg = window.Telegram.WebApp;
-//   if (!tg) return;
+onMounted(() => {
+  const tg = window.Telegram.WebApp;
+  if (!tg) return;
 
-//   tg.ready();
-//   const user = tg.initDataUnsafe.user;
-//   if (user.photo_url) {
-//     avatarUrl.value = user.photo_url;
-//   }
-// });
+  tg.ready();
+  const user = tg.initDataUnsafe.user;
+  if (user.photo_url) {
+    avatarUrl.value = user.photo_url;
+  }
+});
 </script>
 
 <template>
